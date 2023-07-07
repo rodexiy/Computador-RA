d4f2f5e570726592bc42de0046f4c0def2cbf06d
rodexiy <rodexcontato@gmail.com>
2023-07-07 15:44:02 -0300

    mod

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+const card = document.getElementById("card")
+const displaymagem = document.getElementById("card-imagem")
+const descricao = document.getElementById("card-descricao")
 
+let cardImageIndex = 0
+let componenteSelecionado = "Processador"
 
-
-
-let computadores = [
-    [1] = {
+let computadores = {
+    computador0: {
         Componentes: {
             Processador: {
                 Descricao: "aaaaaa",
-                Fotos: []
+                Fotos: [
+                    "Assets/Computador0/Processador/0.webp"
+                ]
             },
             HDD: {
                 Descricao: "aaaaaa",
@@ -35,6 +40,45 @@ let computadores = [
             },            
             
         }
+    },
+}
+
+function computadorEscaneado(idComputador) {
+
+}
+
+
+function bindarImagens() {
+    for (indexImagem = 0; indexImagem < computadores.length; indexImagem++) {
+        let idImagem = "computador" + indexImagem
+        document.getElementById(idImagem).addEventListener("targetFound", event => {
+            computadorEscaneado(idImagem)
+        })
     }
-]
+}
+
+descricao.style.display = "block"
+descricao.textContent = computadores.computador0.Componentes.Processador.Descricao
+displaymagem.src = computadores.computador0.Componentes.Processador.Fotos[0]
+
+bindarImagens()
+
+
+let ladoTexto = true
+function flipCard() {
+    cardImageIndex = 0
+    if (ladoTexto) {
+        ladoTexto = false
+        descricao.style.display = "none"
+        displaymagem.style.display = "block"
+    }else {
+        ladoTexto = true
+        descricao.style.display = "block"
+        displaymagem.style.display = "none"
+    }
+}
+
+card.addEventListener("click", function(){
+    flipCard()
+}) 
 
